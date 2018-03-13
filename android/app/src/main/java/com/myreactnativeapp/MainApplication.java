8c5321490b8c3c53b0a5acf0c05852c51f971beb
Nikita <thehikita@gmail.com>
2018-03-13 13:55:24 +0200

    Codepush fixes android

--- android/app/src/main/java/com/myreactnativeapp/MainApplication.java
+++ android/app/src/main/java/com/myreactnativeapp/MainApplication.java
@@ -3,9 +3,6 @@ package com.myreactnativeapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
-
-import com.microsoft.codepush.react.CodePush;
 import com.microsoft.codepush.react.CodePush;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
@@ -36,10 +33,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
-
-
-            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
